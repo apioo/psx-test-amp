cf7fe9c092e097a5a89ddff3de46ed611e7c4f90
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-10-26 21:28:52 +0200

    use guzzle to send request

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,11 +2,18 @@
 
 namespace App\Tests;
 
-use PSX\Framework\Test\ControllerDbTestCase;
 use App\Api\Population;
+use GuzzleHttp\Client;
+use PSX\Framework\Test\ControllerDbTestCase;
+use PSX\Http\Factory\NativeFactory;
 
 class ApiTestCase extends ControllerDbTestCase
 {
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private static $httpClient;
+
     public function getDataSet()
     {
         return $this->createFlatXMLDataSet(__DIR__ . '/api_fixture.xml');
@@ -18,5 +25,41 @@ class ApiTestCase extends ControllerDbTestCase
             [['ANY'], '/population', Population\Collection::class],
             [['ANY'], '/population/:id', Population\Entity::class],
         );
+    }
+
+    /**
+     * We overwrite this method to actually send an HTTP request instead of 
+     * using an internal request. This is slower but tests the aerys to psx http
+     * communication
+     * 
+     * @param string $uri
+     * @param string $method
+     * @param array $headers
+     * @param null $body
+     * @return \PSX\Http\Response
+     */
+    protected function sendRequest($uri, $method, $headers = array(), $body = null)
+    {
+        $response = self::getHttpClient()->request($method, $uri, [
+            'headers' => $headers,
+            'body'    => $body,
+        ]);
+
+        return NativeFactory::createResponse($response);
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    private static function getHttpClient()
+    {
+        if (self::$httpClient) {
+            return self::$httpClient;
+        }
+
+        return self::$httpClient = new Client([
+            'base_uri'    => 'http://127.0.0.1:8080/',
+            'http_errors' => false,
+        ]);
     }
 }
